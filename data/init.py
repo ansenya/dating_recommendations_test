bd36cc710bf7e6f32fe6a91b6a7a5b96d78241da
ansenya <ars.anoshin@gmail.com>
2025-01-24 11:30:25 +0300

    fixed notebook to run with mongo

--- data/init.py
+++ data/init.py
@@ -8,48 +8,48 @@ import torch
 DATABASE_URL = "mongodb://root:insecure@127.0.0.1:27017"
 client = MongoClient(DATABASE_URL)
 db = client.dating
-
-# Инициализация модели
-if torch.cuda.is_available():
-    model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2', device='cuda')
-else:
-    model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2', device='cpu')
-
-# Удаление старых данных
-db.cities.drop()
-db.users.drop()
-
-# Загрузка данных городов из JSON
-with open('cities.json', 'r') as f:
-    city_data = json.load(f)
-
-# Добавление городов в базу данных
-city_ids = {}
-for city in city_data:
-    result = db.cities.insert_one(city)
-    city_ids[city["address"]] = result.inserted_id
-
-# Загрузка данных пользователей из JSON
-with open('users.json', 'r') as f:
-    user_data = json.load(f)
-
-# Обработка и добавление пользователей в базу данных
-users = []
-for user in user_data:
-    city_name = f"г {user.pop('city')}"
-    city_id = city_ids.get(city_name)
-
-    if city_id:
-        user["city_id"] = city_id
-
-    # Кодирование биографии
-    user["embedded_bio"] = model.encode(user["bio"]).tolist()
-
-    # Добавление временных полей
-    user["created_at"] = datetime.now()
-    user["updated_at"] = datetime.now()
-    user["last_active"] = datetime.now()
-
-    users.append(user)
-
-db.users.insert_many(users)
+print(db.list_collection_names())
+# # Инициализация модели
+# if torch.cuda.is_available():
+#     model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2', device='cuda')
+# else:
+#     model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2', device='cpu')
+#
+# # Удаление старых данных
+# db.cities.drop()
+# db.users.drop()
+#
+# # Загрузка данных городов из JSON
+# with open('cities.json', 'r') as f:
+#     city_data = json.load(f)
+#
+# # Добавление городов в базу данных
+# city_ids = {}
+# for city in city_data:
+#     result = db.cities.insert_one(city)
+#     city_ids[city["address"]] = result.inserted_id
+#
+# # Загрузка данных пользователей из JSON
+# with open('users.json', 'r') as f:
+#     user_data = json.load(f)
+#
+# # Обработка и добавление пользователей в базу данных
+# users = []
+# for user in user_data:
+#     city_name = f"г {user.pop('city')}"
+#     city_id = city_ids.get(city_name)
+#
+#     if city_id:
+#         user["city_id"] = city_id
+#
+#     # Кодирование биографии
+#     user["embedded_bio"] = model.encode(user["bio"]).tolist()
+#
+#     # Добавление временных полей
+#     user["created_at"] = datetime.now()
+#     user["updated_at"] = datetime.now()
+#     user["last_active"] = datetime.now()
+#
+#     users.append(user)
+#
+# db.users.insert_many(users)
